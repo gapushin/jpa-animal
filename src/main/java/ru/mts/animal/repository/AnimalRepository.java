723329a46ffb18ee44a5614964bf3922d497dcfb
gapushin <gapushin@mts.ru>
2024-07-16 13:08:53 +0300

    Дз 15 в процессе

--- src/main/java/ru/mts/animal/repository/AnimalRepository.java
+++ src/main/java/ru/mts/animal/repository/AnimalRepository.java
@@ -1,7 +1,6 @@
 package ru.mts.animal.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import ru.mts.animal.entity.Animal;
 
